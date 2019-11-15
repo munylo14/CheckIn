ef99e5cb869cc1eb1b06099f993de18fbcead307
munylo14 <munylo14@gmail.com>
2019-11-15 17:58:48 -0600

    Added barebones coming soon landing page controller and view, and set root to said view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  resources :welcome
   resources :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

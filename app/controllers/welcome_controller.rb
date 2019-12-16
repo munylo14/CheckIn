eeb52c167c990b84e2c42cb37e66b3f49873ecff
Dustin Garrett <dustin.garrett@stonecroptech.com>
2019-12-15 22:46:16 -0600

    Updated Welcome Controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,23 @@ class WelcomeController < ApplicationController
     @students = Student.all
   end
 
-  def checkin
-  
-  end 
+  def new
+    student = Student.find(params[:student])
+    @checkin_log = student.check_logs.create(check_in_params)
+    if @checkin_log.save
+      redirect_to welcome_index_path
+    else
+      render 'index'
+    end
+  end
 
+  def checkin
+
+  end
+
+  private
+
+  def check_in_params
+    params.require(:student).permit(:checkedin)
+  end
 end
--- app/models/checkin_log.rb
+++ app/models/checkin_log.rb
@@ -1,3 +1,3 @@
 class CheckinLog < ApplicationRecord
-	belongs_to :students
+	belongs_to :student
 end

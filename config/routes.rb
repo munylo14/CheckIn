90b1f350abe9c51a52146bcf970443aed9322b31
munylo14 <munylo14@gmail.com>
2019-12-15 20:06:24 -0600

    added checkin action to index controller and to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :welcome
+
+  post 'welcome/checkin'
+
   resources :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
